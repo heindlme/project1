43c3fc7e5bdc86ca546912b25edfb38fe2877b0d
Megan Heindl <heindlme0588@uwec.edu>
2020-04-09 21:56:51 -0500

    Styled report Added an email section to contact us and a picture in the white space Added a picture to history Adjusted picture sizing in about us

--- js/contact-script.js
+++ js/contact-script.js
@@ -1,15 +1,21 @@
 const message = document.getElementById('message');
+const email = document.getElementById('email');
 const submitButton = document.getElementById('submit-button');
+let messageFlag = false;
+let emailFlag = false;
 
 message.addEventListener('input', () => {
-  if (message.value.length > 0) {
-    submitButton.disabled = false;
-  } else {
-    submitButton.disabled = true;
-  }
+  messageFlag = (message.value.length > 0) ? true : false;
+  submitButton.disabled = (messageFlag || emailFlag) ? false : true;
+});
+
+email.addEventListener('input', () => {
+  emailFlag = (email.value.length > 0) ? true : false;
+  submitButton.disabled = (messageFlag|| emailFlag) ? false : true;
 });
 
 submitButton.addEventListener('click', () => {
   message.value = "";
+  email.value = "";
   submitButton.disabled = true;
 });
